37dc8a02c118d4afeba494cd521a8cb0283ee122
zaunist <y.bz@foxmail.com>
2022-07-29 16:24:37 +0800

    feat: support multiple arch and operation system

--- pkg/install/install.go
+++ pkg/install/install.go
@@ -1,32 +1,45 @@
 package install
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"runtime"
 
 	"github.com/zaunist/k/pkg/conf"
 	"github.com/zaunist/k/pkg/use"
 )
+
+var address string
 
 func Do(version string) {
 	install(version)
 }
 
 func install(version string) {
-	address := "https://dl.k8s.io/release/" + version + "/bin/linux/amd64/kubectl"
+	switch runtime.GOOS {
+	case "linux":
+		address = fmt.Sprintf("https://dl.k8s.io/release/%s/bin/%s/%s/kubectl", version, runtime.GOOS, runtime.GOARCH)
+	case "windows":
+		address = fmt.Sprintf("https://dl.k8s.io/release/%s/bin/%s/%s/kubectl.exe", version, runtime.GOOS, runtime.GOARCH)
+	case "darwin":
+		address = fmt.Sprintf("https://dl.k8s.io/release/%s/bin/%s/%s/kubectl", version, runtime.GOOS, runtime.GOARCH)
+	}
 	path := filepath.Join(conf.VersonDir, version)
 
 	if err := os.MkdirAll(path, os.ModePerm); err != nil {
 		log.Printf("create directory failed: %v", err)
 	}
+
 	f, err := os.OpenFile(filepath.Join(path, "kubectl"), os.O_CREATE|os.O_WRONLY, 0755)
 	if err != nil {
 		log.Fatalf("dowanload error: %v", err)
 	}
 	defer f.Close()
+
 	resp, err := http.Get(address)
 	if err != nil {
 		log.Fatalf("get kubectl error: %v", err)
